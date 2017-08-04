c4c6c8be70f5fdca94df09ac2a057061fd7b12f9
AJIex1s <alexpeclug@mail.ru>
2017-08-04 18:30:04 +0300

    hack for radio valueChanged

--- front-end/src/app/search-form/radio-option/radio-option.component.ts
+++ front-end/src/app/search-form/radio-option/radio-option.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MdRadioChange } from '@angular/material';
 
 @Component({
     moduleId: module.id.toString(),
@@ -11,4 +12,9 @@ export class RadioOptionComponent {
     @Input('name') name: string;
     @Input('selected') selected: boolean;
     @Input('stateControl') stateControl: FormControl;
+
+    //hack couse of there is no md-radio-group(todo - to research ui framework)
+    valueChanged(event: MdRadioChange) {
+        this.stateControl.setValue(event.source.checked);
+    }
 }
